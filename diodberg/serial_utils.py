7482e43bcb1082d73832a5ece031279b5daa99f5
Bhaskar Mookerji <mookerji@gmail.com>
2013-08-06 16:27:48 -0700

    Ported utility for probing DMX addresses over serial.

---
+++ diodberg/serial_utils.py
@@ -0,0 +1,50 @@
+# Utilities for testing the serial protocol on the Raspberry Pi.
+
+import sys
+import time
+try:
+    import serial
+except ImportError as err:
+    sys.stderr.write("Error: failed to import module ({})".format(err))
+
+
+def write_dmx(baudrate = 115200, buf = bytearray([255, 255, 255])):
+    """ Simple test routine for DMX-over-serial, with varying baudrates. The buf is
+    the DMX address space (0 - 512). 
+    TODO: The baudrate on the Pi currently ceilings at 115200 baud. Change back to 
+    250000 baud when fixed on the Pi-side.
+    """
+    assert isinstance(buf, bytearray)
+    num_addresses = 512
+    assert len(buf) <= num_addresses
+    # DMX serial default parameters
+    device_name = "/dev/ttyAMA0"
+    port = serial.Serial(device_name)
+    port.baudrate = baudrate
+    port.bytesize = serial.EIGHTBITS
+    port.parity = serial.PARITY_NONE
+    port.stopbits = serial.STOPBITS_TWO
+    port.timeout = 3.
+    # Write break and mark-after-break
+    port.baudrate = baudrate/2
+    port.write(chr(0))
+    # Write start and then values to address 0, 1, 2 
+    # on universe 0
+    port.baudrate = baudrate
+    port.write(chr(0))
+    port.write(buf)
+    port.close()
+
+
+def probe(num_addresses = 15):
+    empty = bytearray([0]*512)
+    for i in xrange(num_addresses):
+        val = bytearray(empty)
+        val[i] = 255
+        write_dmx(buf = val)
+        print "Address: ", i
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    probe()
